00379fb78e67bfe0bcc847b4a7d8a9a8263836f8
Sreenivasan K <sreeramu@gmail.com>
2018-09-05 02:13:46 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+import './showcase.dart';
+import './workspace.dart';
 
-class MyApp extends StatelessWidget {
+void main() => runApp(new App());
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SpinKit Demo',
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
-      home: new MyHomePage(title: 'PlayGround'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(child: LogoWidget()),
-    );
-  }
-}
-
-final Path quadPath = new Path()
-  ..moveTo(123.0, 12.2)
-  ..relativeLineTo(1.1, -0.2)
-  ..relativeCubicTo(4.2, -0.6, 12.1, 1.3, 3.2, 3.1)
-  // etc all the way around
-  ..close();
-
-class LogoWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new SizedBox(
-      width: 350.0,
-      height: 240.0,
-      child: new CustomPaint(
-        painter: new LogoPainter(),
+      home: new Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Align(
+                child: LayoutBuilder(
+                  builder: (context, _) {
+                    return IconButton(
+                      icon: Icon(Icons.play_circle_filled),
+                      iconSize: 50.0,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => ShowCase(),
+                            fullscreenDialog: true,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+                alignment: Alignment.bottomCenter,
+              ),
+              Positioned.fill(
+                child: Center(
+                  child: WorkSpace(),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
-  }
-}
-
-class LogoPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = new Paint()
-      ..color = Colors.blue
-      ..strokeWidth = 0.0;
-
-    canvas.drawPath(quadPath, paint);
-
-    paint.color = Colors.blue[600];
-    canvas.drawCircle(new Offset(123.4, 56.7), 12.0, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
   }
 }
