fae78e9a33367649e48f1b82f02de5263dd3b4df
Sreenivasan K <sreeramu@gmail.com>
2018-09-04 22:44:03 +0530

    build script

---
+++ script/deploy.sh
@@ -0,0 +1,16 @@
+mkdir $HOME/daily/
+cp -R /home/travis/build/sreeramu/playground/build/app/outputs/apk/app-release.apk $HOME/daily/
+# go to home and setup git
+cd $HOME
+git config --global user.email "sreeramu@gmail.com"
+git config --global user.name "sreeramu"
+  
+git clone --quiet --branch=development https://sreeramu:$GITHUB_API_KEY@github.com/sreeramu/flutter-notes  development > /dev/null
+cd development
+cp -Rf $HOME/daily/*  sample-apk/
+git add -f .
+  # git remote rm origin
+  # git remote add origin https://sreeramu:$GITHUB_API_KEY@github.com/sreeramu/flutter-notes
+git add -f .
+git commit -m "Update Sample Apk [skip ci]"
+git push origin development > /dev/null
