92830da83a11a609e3b8b6c62f8ce040054655f0
Sreenivasan K <sreeramu@gmail.com>
2018-09-14 13:51:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:playground/polygon_clipper.dart';
+import 'package:timeline/model/timeline_model.dart';
+import 'package:timeline/timeline.dart';
 
 import './showcase.dart';
 import './workspace.dart';
@@ -16,8 +18,33 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new ImageTile(),
+      home: new TimelineExample(),
     );
+  }
+}
+
+class TimelineExample extends StatelessWidget {
+
+  final List<TimelineModel> list = [
+    TimelineModel(
+      id: "1",
+      description: "Test 1",
+      title: "Test 1"),
+      TimelineModel(
+      id: "2",
+      description: "Test 2",
+      title: "Test 2")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new TimelineComponent(
+      timelineList: list,
+        // lineColor: Colors.red[200], // Defaults to accent color if not provided
+        // backgroundColor: Colors.black87, // Defaults to white if not provided
+        // headingColor: Colors.black, // Defaults to black if not provided
+        // descriptionColor: Colors.grey, // Defaults to grey if not provided
+      );
   }
 }
 
