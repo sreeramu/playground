afa3831dbec0193d26af1dc9ed47cf38dd3b5b6d
Sreenivasan K <sreeramu@gmail.com>
2018-10-05 18:19:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:playground/polygon_clipper.dart';
-import 'package:timeline/model/timeline_model.dart';
-import 'package:timeline/timeline.dart';
-
-import './showcase.dart';
-import './workspace.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -18,199 +11,53 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(appBar: AppBar(title: const Text('TimeLine')),body:new TimelineExample(),),
-    );
-  }
-}
-
-class TimelineExample extends StatelessWidget {
-
-  final List<TimelineModel> list = [
-    TimelineModel(
-      id: "1",
-      description: "Yesvantpur Jn",
-      title: "Yesvantpur Jn"),
-      TimelineModel(
-      id: "2",
-      description: "Tunkur",
-      title: "Test 2"),
-    TimelineModel(
-      id: "3",
-      description: "Arsikere Jn",
-      title: "Test 3"),
-    TimelineModel(
-      id: "4",
-      description: "Hubli Jn",
-      title: "Test 4"),
-    TimelineModel(
-      id: "5",
-      description: "Dharwar",
-      title: "Test 5"),
-    TimelineModel(
-      id: "6",
-      description: "Test 6",
-      title: "Test 6"),
-    TimelineModel(
-      id: "7",
-      description: "Test 7",
-      title: "Test 7"),
-    TimelineModel(
-      id: "8",
-      description: "Test 8",
-      title: "Test 8"),
-    TimelineModel(
-      id: "9",
-      description: "Test 9",
-      title: "Test 9"),
-    TimelineModel(
-      id: "10",
-      description: "Test 10",
-      title: "Test 10")
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return new TimelineComponent(
-      timelineList: list,
-        // lineColor: Colors.red[200], // Defaults to accent color if not provided
-        // backgroundColor: Colors.black87, // Defaults to white if not provided
-        // headingColor: Colors.black, // Defaults to black if not provided
-        // descriptionColor: Colors.grey, // Defaults to grey if not provided
-      );
-  }
-}
-
-List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(2, 1),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(2, 2),
-  const StaggeredTile.count(1, 2),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(3, 1),
-  const StaggeredTile.count(1, 1),
-  const StaggeredTile.count(4, 1),
-];
-
-List<Widget> _tiles = const <Widget>[
-  const _ImageTile('https://picsum.photos/200/300/?random'),
-  const _ImageTile('https://picsum.photos/201/300/?random'),
-  const _ImageTile('https://picsum.photos/202/300/?random'),
-  const _ImageTile('https://picsum.photos/203/300/?random'),
-  const _ImageTile('https://picsum.photos/204/300/?random'),
-  const _ImageTile('https://picsum.photos/205/300/?random'),
-  const _ImageTile('https://picsum.photos/206/300/?random'),
-  const _ImageTile('https://picsum.photos/207/300/?random'),
-  const _ImageTile('https://picsum.photos/208/300/?random'),
-  const _ImageTile('https://picsum.photos/209/300/?random'),
-];
-
-class ImageTile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(title: const Text('Bottom App Bar')),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.red,
-        onPressed: () {},
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 5.0,
-        elevation: 5.0,
-        child: new BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            title: new Text('Messages'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            title: new Text('Messages'),
-          ),
-        ]),
-      ),
-    );
-  }
-}
-
-class _ImageTile extends StatelessWidget {
-  const _ImageTile(this.gridImage);
-
-  final gridImage;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Card(
-      color: const Color(0x00000000),
-      elevation: 3.0,
-      child: new GestureDetector(
-        onTap: () {
-          print("hello");
-        },
-        child: new Container(
-            decoration: new BoxDecoration(
-          image: new DecorationImage(
-            image: new NetworkImage(gridImage),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: new BorderRadius.all(const Radius.circular(10.0)),
-        )),
-      ),
-    );
-  }
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'SpinKit Demo',
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData(
-        brightness: Brightness.dark,
-      ),
       home: new Scaffold(
-        body: SafeArea(
-          child: Stack(
-            children: <Widget>[
-              Align(
-                child: LayoutBuilder(
-                  builder: (context, _) {
-                    return IconButton(
-                      icon: Icon(Icons.play_circle_filled),
-                      iconSize: 50.0,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute<void>(
-                            builder: (BuildContext context) => ShowCase(),
-                            fullscreenDialog: true,
-                          ),
-                        );
-                      },
-                    );
-                  },
+          key: scaffoldKey,
+          appBar: AppBar(title: const Text('TimeLine')),
+          body: Center(
+            child: Column(children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return GestureDetector(
+                            onTap: () {
+                              scaffoldKey.currentState.showSnackBar(
+                                  new SnackBar(
+                                      content:
+                                          new Text("You pressed Image No")));
+                            },
+                            child: Container(
+                                child: Padding(
+                                    padding: const EdgeInsets.all(32.0),
+                                    child: Text(
+                                        'This is the modal bottom sheet. Tap anywhere to dismiss.',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color:
+                                                Theme.of(context).accentColor,
+                                            fontSize: 24.0)))));
+                      });
+                },
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  "Open-1",
                 ),
-                alignment: Alignment.bottomCenter,
               ),
-              Positioned.fill(
-                child: Center(
-                  child: WorkSpace(),
+              RaisedButton(
+                onPressed: () {},
+                textColor: Colors.white,
+                color: Colors.red,
+                padding: const EdgeInsets.all(8.0),
+                child: new Text(
+                  "Open-2",
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
+              )
+            ]),
+          )),
     );
   }
 }
