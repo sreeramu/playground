da14b20e48bc16c411666b6e7dd30a0e2994fef5
Sreenivasan K <sreeramu@gmail.com>
2018-12-20 15:37:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AddCustomerApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class AddCustomerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
+      title: 'Add Customer',
+      home: AddCustomerForm(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
+class AddCustomerForm extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return HomeScreenState();
-  }
+  _AddCustomerFormState createState() => _AddCustomerFormState();
 }
 
-class HomeScreenState extends State<HomeScreen> {
+class _AddCustomerFormState extends State<AddCustomerForm> {
+  List<String> days = List<String>();
+
+  @override
+  void initState() {
+    super.initState();
+    var today = new DateTime.now();
+    var oneDayDuration = Duration(days: 1);
+    var fiftyDaysFromNow = today.add(new Duration(days: 50));
+    while (today != fiftyDaysFromNow) {
+      days.add(today.day.toString());
+      today.add(oneDayDuration);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("PlayGround"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Customer'),
+      ),
+      body: Center(
+        child: ListView(
+            // This next line does the trick.
+            scrollDirection: Axis.horizontal,
+            children: getListWidget()),
+      ),
     );
+  }
+
+  List<Widget> getListWidget() {
+    List<Widget> widgetList = List<Widget>();
+    days.forEach((f) {
+      widgetList.add(Container(
+        width: 160.0,
+        height: 50.0,
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Text(
+          f,
+          style: TextStyle(color: Colors.white),
+        ),
+      ));
+    });
+    return widgetList;
   }
 }
