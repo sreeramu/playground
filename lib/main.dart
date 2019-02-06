656ce1208f1e1dbc0c830f835f1be74a984cd8d7
Sreenivasan K <sreeramu@gmail.com>
2019-02-06 12:11:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,25 @@ class AddCustomerForm extends StatefulWidget {
 }
 
 class _AddCustomerFormState extends State<AddCustomerForm> {
+  double _fraction = 0.0;
+  Animation<double> animation;
+ 
   @override
   void initState() {
     super.initState();
+    var controller = AnimationController(
+        duration: Duration(milliseconds: 1000), vsync: this);
+ 
+    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          _fraction = animation.value;
+        });
+      });
+ 
+    controller.forward();
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +46,34 @@ class _AddCustomerFormState extends State<AddCustomerForm> {
         title: Text('Customer'),
       ),
       backgroundColor: Colors.grey,
-      body: Container(child: StickyDateList()),
+      body: Center(child: CustomPaint(painter: CrossPainter(_fraction))),
     );
+  }
+}
+
+class CirclePainter extends CustomPainter {
+  Paint _paint;
+  double _fraction;
+ 
+  CirclePainter(this._fraction) {
+    _paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 10.0
+      ..strokeCap = StrokeCap.round
+        ..style = PaintingStyle.stroke;
+  }
+ 
+  @override
+  void paint(Canvas canvas, Size size) {
+    print('paint $_fraction');
+ 
+    var rect = Offset(0.0, 0.0) & size;
+ 
+    canvas.drawArc(rect, -pi / 2, pi * 2 * _fraction, false, _paint);
+  }
+ 
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) {
+    return oldDelegate._fraction != _fraction;
   }
 }
